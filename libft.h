ea79d77527ca22a9c911380e7590acf9c4b9dfd6
Mathis PANDELE <mapandel@e3r5p10.42.fr>
2016-12-05 16:20:10 +0100

    libft.h 1.1 : Modification norme

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 02:57:54 by mapandel          #+#    #+#             */
-/*   Updated: 2016/12/05 15:58:48 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/05 16:17:18 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void				ft_putstr_fd(char const *s, int fd);
 char				*ft_strcat(char *dest, const char *src);
 char				*ft_strccpy(char *dest, const char *src, char c);
 char				*ft_strchr(const char *s, int c);
-size_t			ft_strclen(const char *s, char c);
+size_t				ft_strclen(const char *s, char c);
 void				ft_strclr(char *s);
 int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strcpy(char *dest, const char *src);
