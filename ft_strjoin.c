a088d84755e80070e886243ed97b4da007c8f461
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 23:52:01 +0100

    ft_strjoin.c 1.1 : Optimisation

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 19:17:29 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/27 03:37:39 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 23:47:10 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len = 0;
 	result = NULL;
-	if (s1 == NULL || s2 == NULL)
+	if (!s1 || !s2)
 		return (NULL);
 	len = ft_strlen(s1);
 	len += ft_strlen(s2);
-	if ((result = ft_strnew(len)) == NULL)
+	if (!(result = ft_strnew(len)))
 		return (NULL);
 	result = ft_strcpy(result, s1);
 	result = ft_strcat(result, s2);
