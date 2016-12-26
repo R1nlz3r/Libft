e126c8b3a28fad26494fc8f5110d452fcf437a48
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 23:36:56 +0100

    ft_strncpy.c 1.1 : Optimisation

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 07:03:44 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/04 07:10:53 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 23:21:59 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@ char	*ft_strncpy(char *dest, const char *src, size_t n)
 	while (i < n && src[i])
 	{
 		dest[i] = src[i];
-		i++;
+		++i;
 	}
 	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+		dest[i++] = '\0';
 	return (dest);
 }
