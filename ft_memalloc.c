b42570dd6d35bec2d4cd638bec1d2d95659cab5e
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 16:13:04 +0100

    ft_memalloc.c 1.1 : Optimisation

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 17:36:48 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/15 19:25:33 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 15:11:20 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memalloc(size_t size)
 	void	*bus;
 
 	bus = malloc(size);
-	if (bus == NULL)
+	if (!bus)
 		return (NULL);
 	ft_bzero(bus, size);
 	return (bus);
