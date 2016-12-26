7e878c37a181f4ae32b04c3b1730f407b629cc4f
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 16:13:29 +0100

    ft_memccpy.c 1.1 : Optimisation

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 05:00:09 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/15 14:55:42 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 15:14:12 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 		((char*)dest)[i] = ((char*)(unsigned long)src)[i];
 		i++;
 	}
-	if (i < n)
+	if (i < n && ((char*)(unsigned long)src)[i] == (char)c)
 	{
-		if (((char*)(unsigned long)src)[i] == (char)c)
-		{
-			((char*)dest)[i] = ((char*)(unsigned long)src)[i];
-			return (&((char*)dest)[i + 1]);
-		}
+		((char*)dest)[i] = ((char*)(unsigned long)src)[i];
+		return (&((char*)dest)[i + 1]);
 	}
 	return (NULL);
 }
