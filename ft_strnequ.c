bc7b7f75123741738bdb7737867958a7d5bb84b1
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 23:37:12 +0100

    ft_strnequ.c 1.1 : Optimisation

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 18:47:30 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/12 15:36:29 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 23:15:00 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (s1 != NULL && s2 != NULL)
-	{
-		if (ft_strncmp(s1, s2, n) == 0)
-			return (1);
-	}
+	if (s1 && s2 && !ft_strncmp(s1, s2, n))
+		return (1);
 	return (0);
 }
