6884e57bd22db9adda0bef0df864c1f7817750ff
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 16:11:38 +0100

    ft_lstiter.c 1.1 : Optimisation

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 02:40:02 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/09 22:06:51 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 14:29:51 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
 	t_list		*next;
 
-	if (f && lst)
+	while (f && lst)
 	{
-		while (lst)
-		{
-			next = lst->next;
-			f(lst);
-			lst = next;
-		}
+		next = lst->next;
+		f(lst);
+		lst = next;
 	}
 }
