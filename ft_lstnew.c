0401ceb93a35c0f453be84e2819ca770c7147e92
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 16:12:15 +0100

    ft_lstnew.c 1.1 : Optimisation

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 23:38:12 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/09 21:36:02 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 14:35:45 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	t_list		*new;
 
 	new = NULL;
-	if ((new = (t_list*)malloc(sizeof(t_list))) == NULL)
+	if (!(new = (t_list*)malloc(sizeof(t_list))))
 		return (NULL);
-	if (content == NULL)
+	if (!content)
 	{
 		new->content = NULL;
 		new->content_size = 0;
 	}
 	else
 	{
-		if ((new->content = malloc(content_size)) == NULL)
+		if (!(new->content = malloc(content_size)))
 			return (NULL);
 		new->content = ft_memcpy(new->content, content, content_size);
 		new->content_size = content_size;
