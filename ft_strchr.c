6255cfd80953331f5ea3f8da69f700897954ee19
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 16:25:34 +0100

    ft_strchr.c 1.1 : Optimisation

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 07:50:01 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/05 17:39:51 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 15:54:57 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	size_t		i;
 
 	i = 0;
-	while (s[i] != c && s[i])
-		i++;
+	while (s[i] && s[i] != c)
+		++i;
 	if (s[i] == c)
 		return (&((char*)(unsigned long)s)[i]);
-	else
-		return (NULL);
+	return (NULL);
 }
