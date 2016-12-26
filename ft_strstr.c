b06476b4181ce5c48f476f244a3e3203dba2c947
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 23:38:38 +0100

    ft_strstr.c 1.1 : Optimisation

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 08:12:14 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/09 22:09:13 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 23:10:27 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ char	*ft_strstr(const char *big, const char *little)
 	len = ft_strlen(little);
 	while (*big)
 	{
-		if (*big == *little && (ft_memcmp(big, little, len) == 0))
+		if (*big == *little && !(ft_memcmp(big, little, len)))
 			return ((char*)(unsigned long)big);
-		big++;
+		++big;
 	}
 	return (NULL);
 }
