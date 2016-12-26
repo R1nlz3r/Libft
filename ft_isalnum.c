aa0d9e64f04042b18b8c46e2780f0e704d8d79e1
Mathis Pandelé <r1nlz3r@MacBook-Pro-de-Mathis.local>
2016-12-26 16:03:25 +0100

    ft_isalnum.c 1.1 : Optimisation

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 19:08:42 by mapandel          #+#    #+#             */
-/*   Updated: 2016/11/05 19:09:51 by mapandel         ###   ########.fr       */
+/*   Updated: 2016/12/26 13:11:15 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@ int		ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
-	else
-		return (0);
+	return (0);
 }
