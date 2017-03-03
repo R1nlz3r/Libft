072ec6543b72a9bf221ffa0ee03f740f649a58cb
Mathis Pandelé <mapandel@student.42.fr>
2017-03-03 20:55:24 +0100

    ft_strjoin_leakless.c 1.1 : Fix

--- ft_strjoin_leakless.c
+++ ft_strjoin_leakless.c
@@ -6,13 +6,13 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 14:32:12 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/24 14:32:51 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/02/24 16:07:29 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin_leakless(char const *s1, char const *s2)
+char	*ft_strjoin_leakless(char *s1, char const *s2)
 {
 	size_t		len;
 	char		*result;
@@ -27,6 +27,6 @@ char	*ft_strjoin_leakless(char const *s1, char const *s2)
 		return (NULL);
 	result = ft_strcpy(result, s1);
 	result = ft_strcat(result, s2);
-	ft_memdel((void**)s1);
+	ft_memdel((void**)&s1);
 	return (result);
 }
