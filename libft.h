a8cd8580bd81a9c95cf2420c9536c8c40fad5342
Mathis Pandelé <mapandel@student.42.fr>
2017-03-03 20:57:49 +0100

    libft.h 1.17 : Nouveaux prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 02:57:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/24 14:35:24 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/03 20:45:22 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ typedef struct		s_list
 
 int					ft_atoi(const char *nptr);
 void				ft_bzero(void *s, size_t n);
-char				*ft_ftoabase(long double nbr, int base);
 int					ft_isalnum(int c);
 int					ft_isalpha(int c);
 int					ft_isascii(int c);
@@ -34,7 +33,10 @@ int					ft_isblank(int c);
 int					ft_iscntrl(int c);
 int					ft_isdigit(int c);
 int					ft_isgraph(int c);
+int					ft_isinf(double f);
+int					ft_islessinf(double f);
 int					ft_islower(int c);
+int					ft_isnan(double f);
 int					ft_isprint(int c);
 int					ft_ispunct(int c);
 int					ft_isspace(int c);
@@ -61,20 +63,21 @@ void				ft_putchar(char c);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putendl(char const *s);
 void				ft_putendl_fd(char const *s, int fd);
+void				ft_putllnbr(long long n);
 void				ft_putmap(char **m, size_t nblines);
 void				ft_putnbr(int n);
 void				ft_putnbr_fd(int n, int fd);
 void				ft_putstr(char const *s);
 void				ft_putstr_fd(char const *s, int fd);
 char				*ft_stradd(char const *s1, char c);
-char				*ft_stradd_leakless(char const *s1, char c);
+char				*ft_stradd_leakless(char *s1, char c);
 char				*ft_strcat(char *dest, const char *src);
 char				*ft_strccat(char *dest, const char *src, char c);
 int					ft_strccmp(const char *s1, const char *s2, char c);
 char				*ft_strccpy(char *dest, const char *src, char c);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strcjoin(char const *s1, char const *s2, char c);
-char				*ft_strcjoin_leakless(char const *s1, char const *s2,
+char				*ft_strcjoin_leakless(char *s1, char const *s2,
 	char c);
 size_t				ft_strclen(const char *s, char c);
 void				ft_strclr(char *s);
@@ -87,7 +90,7 @@ char				*ft_strfill(char *s, char c, size_t size);
 void				ft_striter(char *s, void (*f)(char*));
 void				ft_striteri(char *s, void (*f)(unsigned int, char*));
 char				*ft_strjoin(char const *s1, char const *s2);
-char				*ft_strjoin_leakless(char const *s1, char const *s2);
+char				*ft_strjoin_leakless(char *s1, char const *s2);
 size_t				ft_strlcat(char *dest, const char *src, size_t size);
 size_t				ft_strlen(const char *s);
 char				*ft_strmap(char const *s, char (*f)(char));
@@ -102,6 +105,7 @@ char				*ft_strnstr(const char *big, const char *little,
 char				*ft_strplugc(const char *s, char tochange, char toadd);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strrev(const char *s);
+char				*ft_strrev_leakless(char *s);
 char				**ft_strsplit(char const *s, char c);
 char				*ft_strstr(const char *big, const char *little);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
