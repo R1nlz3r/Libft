50699a74c7a1ceec943c1a54b1594dbb786e1df0
Mathis Pandelé <mapandel@student.42.fr>
2017-03-03 20:54:06 +0100

    ft_stradd_leakless.c 1.1 : Fix

--- ft_stradd_leakless.c
+++ ft_stradd_leakless.c
@@ -6,13 +6,13 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 14:25:29 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/24 14:26:32 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/02/24 16:07:08 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_stradd_leakless(char const *s1, char c)
+char	*ft_stradd_leakless(char *s1, char c)
 {
 	size_t		len;
 	char		*result;
@@ -26,6 +26,6 @@ char	*ft_stradd_leakless(char const *s1, char c)
 		return (NULL);
 	result = ft_strcpy(result, s1);
 	result[len - 1] = c;
-	ft_memdel((void**)s1);
+	ft_memdel((void**)&s1);
 	return (result);
 }
