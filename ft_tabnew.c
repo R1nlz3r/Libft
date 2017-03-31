5111b875b8b1f1c7517dc5c93efbee597a055a89
Mathis Pandelé <mapandel@student.42.fr>
2017-04-01 01:40:38 +0200

    ft_tabnew.c 1.2 : Changement tampon

--- ft_tabnew.c
+++ ft_tabnew.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 14:45:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/18 15:06:56 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/01 01:18:23 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int		*ft_tabnew(size_t size)
 	int			*tab;
 
 	i = 0;
-	if (!(tab = (int*)malloc(size)))
+	if (!(tab = (int*)malloc(size + 1)))
 		return (NULL);
-	while (i < size)
+	while (i <= size)
 		tab[i++] = 0;
 	return (tab);
 }
