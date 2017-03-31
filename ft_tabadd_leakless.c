f53d58467cf66a99c72a4f2645975dc25ed85e0f
Mathis Pandelé <mapandel@student.42.fr>
2017-04-01 01:37:38 +0200

    ft_tabadd_leakless.c 1.0 : Implementation

---
+++ ft_tabadd_leakless.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabadd_leakless.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/01 01:19:33 by mapandel          #+#    #+#             */
+/*   Updated: 2017/04/01 01:32:37 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		*ft_tabadd_leakless(int *t, int i)
+{
+	size_t		len;
+	int			*result;
+
+	len = 0;
+	result = NULL;
+	if (!t)
+		return (NULL);
+	len = ft_tablen(t) + 2;
+	if (!(result = ft_tabnew(len)))
+		return (NULL);
+	result = ft_tabcpy(result, t);
+	result[len - 1] = i;
+	ft_memdel((void**)&t);
+	return (result);
+}
