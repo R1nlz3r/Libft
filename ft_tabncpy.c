ccb84be2e79594f45ca4e1f5461165359013ed28
Mathis Pandelé <mapandel@student.42.fr>
2017-04-01 03:57:42 +0200

    ft_tabadd_leakless.c 1.3 : Fix

--- ft_tabncpy.c
+++ ft_tabncpy.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/01 03:37:31 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/01 03:38:21 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/01 03:57:28 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		*ft_tabncpy(int *dest, const int *src, size_t n)
 	size_t i;
 
 	i = 0;
-	while (i < n && src[i] != 0)
+	while (i < n)
 	{
 		dest[i] = src[i];
 		++i;
