c77986050f0b5ad5d55420659f3ca39fc40f00ec
Mathis Pandelé <mapandel@student.42.fr>
2017-04-01 04:02:18 +0200

    ft_tabadd_leakless.c 1.4 : Fix

--- ft_tabadd_leakless.c
+++ ft_tabadd_leakless.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/01 01:19:33 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/01 03:55:15 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/01 04:02:03 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int		*ft_tabadd_leakless(int *t, int i)
 	if (!(result = ft_tabnew(len)))
 		return (NULL);
 	result = ft_tabncpy(result, t, len - 2);
-	result[len - 1] = i;
-	result[len] = 0;
+	result[len - 2] = i;
+	result[len - 1] = 0;
 	ft_memdel((void**)&t);
 	return (result);
 }
