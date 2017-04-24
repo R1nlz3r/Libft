1a14fa3ffb39fe767f3a6776a48d18a2574caf5a
Mathis Pandelé <mapandel@student.42.fr>
2017-04-25 01:02:27 +0200

    ft_wstrcpy.c 1.1 : Fix

--- ft_wstrcpy.c
+++ ft_wstrcpy.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 05:20:17 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/18 05:20:55 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/18 06:00:40 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ wchar_t			*ft_wstrcpy(wchar_t *dest, const wchar_t *src)
 		dest[i] = src[i];
 		++i;
 	}
-	dest[i] = '\0';
+	dest[i] = L'\0';
 	return (dest);
 }
