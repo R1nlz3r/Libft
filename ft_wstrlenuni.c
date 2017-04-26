752fe8a4f56d316eb965df24aae5e7774046255a
Mathis Pandelé <mapandel@student.42.fr>
2017-04-26 23:53:53 +0200

    ft_wstrlenuni.c 1.1 : Fix

--- ft_wstrlenuni.c
+++ ft_wstrlenuni.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/25 00:40:36 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/25 00:41:29 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/26 23:52:12 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 size_t		ft_wstrlenuni(const wchar_t *ws)
 {
 	size_t	i;
+	size_t	ret;
 
 	i = 0;
+	ret = 0;
 	while (ws[i])
 	{
 		if (ws[i] < 128)
-			++i;
+			++ret;
 		else if (ws[i] >= 128 && ws[i] < 2048)
-			i += 2;
+			ret += 2;
 		else if (ws[i] >= 2048 && ws[i] < 65536)
-			i += 3;
+			ret += 3;
 		else if (ws[i] >= 65536 && ws[i] <= 1114111)
-			i += 4;
-		else
-			++i;
+			ret += 4;
+		++i;
 	}
-	return (i);
+	return (ret);
 }
