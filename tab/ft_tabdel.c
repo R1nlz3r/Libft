30b6129c384d6c3386a74960f415e1663a8f4983
Mathis Pandelé <mapandel@student.42.fr>
2017-06-01 01:52:52 +0200

    ft_tabdel.c 2.1 : Work for t_tab

--- tab/ft_tabdel.c
+++ tab/ft_tabdel.c
@@ -6,15 +6,20 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 12:09:11 by mapandel          #+#    #+#             */
-/*   Updated: 2017/05/05 12:09:33 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/01 01:44:48 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_tabdel(int **as)
+void	ft_tabdel(t_tab **t)
 {
-	if (as)
-		free(*as);
-	*as = NULL;
+	if (t && (*t)->tab)
+	{
+		free((*t)->tab);
+		(*t)->tab = NULL;
+	}
+	if (t)
+		free(*t);
+	*t = NULL;
 }
