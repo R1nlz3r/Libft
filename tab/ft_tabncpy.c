3e5a1dd1fc10c4a7bbe917b7b4382ef9145f943f
Mathis Pandelé <mapandel@student.42.fr>
2017-06-02 01:48:08 +0200

    ft_tabncpy.c 2.2 : Work for t_tab->len

--- tab/ft_tabncpy.c
+++ tab/ft_tabncpy.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/01 03:37:31 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/01 01:45:08 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/01 17:58:03 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ t_tab		*ft_tabncpy(t_tab *dest, const t_tab *src, size_t n)
 	size_t i;
 
 	i = 0;
-	while (i < n && i < src->size)
+	while (i < n && i < src->len)
 	{
 		dest->tab[i] = src->tab[i];
 		++i;
 	}
 	while (i < n)
 		dest->tab[i++] = 0;
+	if (n > dest->len)
+		dest->len = n;
 	return (dest);
 }
