c96de2f7d085fba7c4553434731f3a1ca5ae327a
Mathis Pandelé <mapandel@student.42.fr>
2017-06-02 01:48:43 +0200

    ft_tabnew.c 2.2 : Work for t_tab->len

--- tab/ft_tabnew.c
+++ tab/ft_tabnew.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 14:45:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/01 01:45:26 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/01 17:40:54 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ t_tab			*ft_tabnew(size_t size)
 		|| !(t->tab = (int*)malloc(sizeof(int) * size)))
 		return (NULL);
 	t->size = size;
+	t->len = 0;
 	while (size)
 		t->tab[--size] = 0;
 	return (t);
