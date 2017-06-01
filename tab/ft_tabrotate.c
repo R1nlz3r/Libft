d12e66630cfa5d8b891f01de1161f88e34eee8ac
Mathis Pandelé <mapandel@student.42.fr>
2017-06-02 01:50:16 +0200

    ft_tabrotate.c 2.0 : Implementation

---
+++ tab/ft_tabrotate.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabrotate.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/01 23:32:42 by mapandel          #+#    #+#             */
+/*   Updated: 2017/06/02 01:41:11 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_tab		*ft_tabrotate(t_tab *t)
+{
+	int		tmp;
+	int		tmp2;
+	size_t	i;
+
+	i = 1;
+	if (!t || !t->len || t->size < t->len)
+		return (t);
+	tmp = t->tab[0];
+	t->tab[0] = t->tab[t->len - 1];
+	while (i < t->len - 1)
+	{
+		tmp2 = t->tab[i];
+		t->tab[i++] = tmp;
+		tmp = tmp2;
+	}
+	return (t);
+}
