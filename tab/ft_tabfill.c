e4d3df9657812ebf7596c4584857a0785a33204e
Mathis Pandelé <mapandel@student.42.fr>
2017-06-02 01:48:28 +0200

    ft_tabfill.c 2.2 : Work for t_tab->len

--- tab/ft_tabfill.c
+++ tab/ft_tabfill.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 09:27:50 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/01 01:35:43 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/01 18:02:20 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_tab		*ft_tabfill(t_tab *t, int n, size_t size)
 {
+	if (size > t->len)
+		t->len = size;
 	while (size)
 		t->tab[--size] = n;
 	return (t);
