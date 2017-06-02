743bb4a651983dd51f08275a7436bbf63b848352
Mathis Pandelé <mapandel@student.42.fr>
2017-06-02 07:16:41 +0200

    ft_tabpush.c 2.1 : Fix

--- tab/ft_tabpush.c
+++ tab/ft_tabpush.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/01 02:09:03 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/02 01:37:52 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/02 07:06:18 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void		ft_tabpush(t_tab *a, t_tab *b)
 {
-	if (!a->len || a->size <= a->len || b->size <= b->len)
+	if (!a->len || a->size < a->len || b->size <= b->len)
 		return ;
 	b->tab[b->len++] = a->tab[a->len-- - 1];
 }
