bf66f7262e3ed73675673d16787c65d8902e17e2
Mathis Pandelé <mapandel@student.42.fr>
2017-06-07 02:21:36 +0200

    ft_tabmedian.c 2.1 : Fix

--- tab/ft_tabmedian.c
+++ tab/ft_tabmedian.c
@@ -6,16 +6,16 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 18:12:34 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/06 18:34:04 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/07 02:20:08 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_tabmedian(t_tab *t)
+double			ft_tabmedian(t_tab *t)
 {
 	t_tab	*sort;
-	int		result;
+	double	result;
 	size_t	i;
 
 	i = 0;
@@ -28,7 +28,7 @@ int			ft_tabmedian(t_tab *t)
 	if (sort->len % 2)
 		result = sort->tab[i];
 	else
-		result = (sort->tab[i] + sort->tab[i + 1]) / 2;
+		result = (sort->tab[i] + sort->tab[i - 1]) / 2;
 	ft_tabdel(&sort);
 	return (result);
 }
