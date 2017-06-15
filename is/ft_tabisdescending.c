d2770204b9fa9d80629e013ced6db34a5d037b19
Mathis Pandelé <mapandel@student.42.fr>
2017-06-16 01:49:15 +0200

    ft_tabisdescending.c 2.1 : Fix

--- is/ft_tabisdescending.c
+++ is/ft_tabisdescending.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/02 17:12:22 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/06 06:34:39 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/15 22:45:29 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ int		ft_tabisdescending(t_tab *t)
 	size_t		i;
 
 	i = 0;
-	if (t->len < 2)
+	if (t->len < 1)
 		return (0);
+	else if (t->len == 1)
+		return (1);
 	while (i < t->len - 1)
 	{
 		if (t->tab[i] < t->tab[i + 1])
