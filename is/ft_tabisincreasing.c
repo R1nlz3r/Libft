f2f4e4545a16f6e3307cfbbb72fad06177f7ede3
Mathis Pandelé <mapandel@student.42.fr>
2017-06-16 01:49:41 +0200

    ft_tabisincreasing.c 2.1 : Fix

--- is/ft_tabisincreasing.c
+++ is/ft_tabisincreasing.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 06:33:05 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/06 06:34:34 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/15 22:45:54 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ int		ft_tabisincreasing(t_tab *t)
 	size_t		i;
 
 	i = 0;
-	if (t->len < 2)
+	if (t->len < 1)
 		return (0);
+	else if (t->len == 1)
+		return (1);
 	while (i < t->len - 1)
 	{
 		if (t->tab[i] > t->tab[i + 1])
