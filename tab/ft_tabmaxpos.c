c005ab4d8a49d49925493844511f8b83a47578b2
Mathis Pandelé <mapandel@student.42.fr>
2017-06-16 11:19:37 +0200

    ft_tabmaxpos.c 2.1 : Fix

--- tab/ft_tabmaxpos.c
+++ tab/ft_tabmaxpos.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/15 05:38:30 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/15 05:43:27 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/16 11:06:00 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,23 @@
 
 size_t		ft_tabmaxpos(t_tab *t)
 {
-	size_t	i;
 	int		max;
+	size_t	max_i;
+	size_t	i;
 
+	max_i = 0;
 	i = 0;
 	if (!t || !t->len || t->size < t->len)
 		return (0);
-	max = ft_tabmax(t);
-	while (t->tab[i] != max)
+	max = t->tab[0];
+	while (i < t->len)
+	{
+		if (max < t->tab[i])
+		{
+			max = t->tab[i];
+			max_i = i;
+		}
 		++i;
-	return (i);
+	}
+	return (max_i);
 }
