63f9925f96c403db609b9d3410365c2c73c98197
Mathis Pandelé <mapandel@student.42.fr>
2017-06-21 06:13:57 +0200

    ft_tabfindpos_overeq.c 1.0 : Implementation

---
+++ tab/ft_tabfindpos_overeq.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabfindpos_overeq.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/21 02:44:00 by mapandel          #+#    #+#             */
+/*   Updated: 2017/06/21 03:15:13 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		ft_tabfindpos_overeq(t_tab *t, int n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!t || !t->len || t->size < t->len)
+		return (0);
+	while (i < t->len && t->tab[i] < n)
+		++i;
+	if (i == t->len)
+		return (0);
+	return (i);
+}
