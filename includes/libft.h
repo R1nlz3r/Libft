82bae4e18ba8de7aca19f4169e0bd683ea7c4bbb
Mathis Pandelé <mapandel@student.42.fr>
2017-06-21 06:13:19 +0200

    libft.h 2.8 : New prototypes

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 02:57:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/06/16 11:02:24 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/06/21 06:10:47 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,6 +160,16 @@ t_tab				*ft_tabcpy(t_tab *dest, const t_tab *src);
 void				ft_tabdel(t_tab **t);
 t_tab				*ft_tabdup(t_tab *t);
 t_tab				*ft_tabfill(t_tab *t, int n, size_t size);
+size_t				ft_tabfindpos(t_tab *t, int n);
+size_t				ft_tabfindpos_over(t_tab *t, int n);
+size_t				ft_tabfindpos_overeq(t_tab *t, int n);
+size_t				ft_tabfindpos_under(t_tab *t, int n);
+size_t				ft_tabfindpos_undereq(t_tab *t, int n);
+size_t				ft_tabfindrev(t_tab *t, int n);
+size_t				ft_tabfindrev_over(t_tab *t, int n);
+size_t				ft_tabfindrev_overeq(t_tab *t, int n);
+size_t				ft_tabfindrev_under(t_tab *t, int n);
+size_t				ft_tabfindrev_undereq(t_tab *t, int n);
 int					ft_tabmax(t_tab *t);
 int					ft_tabmax_underone(t_tab *t);
 size_t				ft_tabmaxpos(t_tab *t);
