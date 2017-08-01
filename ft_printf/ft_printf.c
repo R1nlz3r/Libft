3de3db65c688b7990e573e7331379929277065cb
Mathis Pandelé <mapandel@student.42.fr>
2017-08-01 05:00:29 +0200

    ft_printf.c 2.0 : Implementation

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/30 10:21:36 by mapandel          #+#    #+#             */
+/*   Updated: 2017/05/02 15:05:34 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_printf(char *format, ...)
+{
+	t_printf		*p;
+	int				ret;
+
+	p = NULL;
+	ret = 0;
+	if (!format || !(p = init_t_printf(p)))
+		return (-1);
+	va_start(p->ap, format);
+	ft_printf_display(p, format);
+	va_end(p->ap);
+	ret = p->ret;
+	if (p->error == 1)
+		ret = -1;
+	reset_t_printf(p);
+	del_t_printf(p);
+	return (ret);
+}
