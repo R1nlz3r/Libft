fa49f0ecf165439fa859e5b06e95d47f917805f3
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 00:40:11 +0200

    ft_stradd_leakless.c 2.1 : Working with null parameter

--- str/ft_stradd_leakless.c
+++ str/ft_stradd_leakless.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 14:25:29 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/24 16:07:08 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/02 04:21:21 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,12 @@ char	*ft_stradd_leakless(char *s1, char c)
 
 	len = 0;
 	result = NULL;
-	if (!s1)
-		return (NULL);
-	len = ft_strlen(s1) + 1;
+	len = ft_strlen_nullcrashless(s1) + 1;
 	if (!(result = ft_strnew(len)))
 		return (NULL);
 	result = ft_strcpy(result, s1);
 	result[len - 1] = c;
-	ft_memdel((void**)&s1);
+	if (s1)
+		ft_memdel((void**)&s1);
 	return (result);
 }
