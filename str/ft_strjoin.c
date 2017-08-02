b4c758eb1008d6dd65cf4ec223ead4031805219e
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 00:41:53 +0200

    ft_strjoin.c 2.1 : Working with null parameters

--- str/ft_strjoin.c
+++ str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 19:17:29 by mapandel          #+#    #+#             */
-/*   Updated: 2016/12/26 23:47:10 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/02 05:17:46 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len = 0;
 	result = NULL;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1);
-	len += ft_strlen(s2);
+	len = ft_strlen_nullcrashless(s1);
+	len += ft_strlen_nullcrashless(s2);
 	if (!(result = ft_strnew(len)))
 		return (NULL);
-	result = ft_strcpy(result, s1);
-	result = ft_strcat(result, s2);
+	result = ft_strcpy_nullcrashless(result, s1);
+	result = ft_strcat_nullcrashless(result, s2);
 	return (result);
 }
