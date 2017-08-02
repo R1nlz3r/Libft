9ded0c1522ccade3416816b33668b2266b61154f
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 00:33:28 +0200

    ft_putchar_buffer.c 2.0 : Implementation

---
+++ put/ft_putchar_buffer.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_buffer.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 03:56:55 by mapandel          #+#    #+#             */
+/*   Updated: 2017/08/02 04:29:58 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_putchar_buffer(char c, int release)
+{
+	static char		*buf;
+
+	if (!(buf = ft_stradd_leakless(buf, c)))
+		return (1);
+	if (release)
+	{
+		ft_putstr(buf);
+		ft_strdel(&buf);
+	}
+	return (0);
+}
