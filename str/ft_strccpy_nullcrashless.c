81de9916980c2c828695f2aca1f5e8222550ab6e
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 01:51:29 +0200

    ft_strccpy_nullcrashless.c 2.0 : Implementation

---
+++ str/ft_strccpy_nullcrashless.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strccpy_nullcrashless.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 05:15:16 by mapandel          #+#    #+#             */
+/*   Updated: 2017/08/02 05:15:52 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strccpy_nullcrashless(char *dest, const char *src, char c)
+{
+	size_t i;
+
+	i = 0;
+	if (!src)
+		return (dest);
+	while (src[i] && src[i] != c)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
