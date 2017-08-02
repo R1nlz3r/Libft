9d9aa8d4ef752c53425a744a0792b942b5cf1fb0
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 00:34:29 +0200

    ft_putendl_fd_buffer.c 2.0 : Implementation

---
+++ put/ft_putendl_fd_buffer.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd_buffer.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 06:33:20 by mapandel          #+#    #+#             */
+/*   Updated: 2017/08/02 06:34:42 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_putendl_fd_buffer(char *s, int release, int fd)
+{
+	static char		*buf;
+
+	if (!(buf = ft_strjoin_leakless(buf, s)))
+		return (1);
+	if (release)
+	{
+		ft_putendl_fd(buf, fd);
+		ft_strdel(&buf);
+	}
+	return (0);
+}
