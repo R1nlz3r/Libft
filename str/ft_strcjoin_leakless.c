24d31a411b193618ad2554340458770defa11d95
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 02:03:52 +0200

    ft_strcjoin_leakless.c 2.1 : Null parameters work

--- str/ft_strcjoin_leakless.c
+++ str/ft_strcjoin_leakless.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 14:34:17 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/24 16:08:41 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/03 01:45:42 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,13 @@ char	*ft_strcjoin_leakless(char *s1, char const *s2, char c)
 	size_t		len;
 	char		*result;
 
-	len = 0;
 	result = NULL;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1);
-	len += ft_strclen(s2, c);
+	len = ft_strlen_nullcrashless(s1);
+	len += ft_strclen_nullcrashless(s2, c);
 	if (!(result = ft_strnew(len)))
 		return (NULL);
-	result = ft_strcpy(result, s1);
-	result = ft_strccat(result, s2, c);
-	ft_memdel((void**)&s1);
+	result = ft_strcpy_nullcrashless(result, s1);
+	result = ft_strccat_nullcrashless(result, s2, c);
+	ft_strdel(&s1);
 	return (result);
 }
