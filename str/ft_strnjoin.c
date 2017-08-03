8306866a55e6d6f274ec6ca4981670963effe887
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 02:03:33 +0200

    ft_strnjoin.c 2.1 : Null parameters work

--- str/ft_strnjoin.c
+++ str/ft_strnjoin.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/01 13:02:22 by mapandel          #+#    #+#             */
-/*   Updated: 2017/05/01 13:05:28 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/03 01:42:11 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
 	size_t		len;
 	char		*result;
 
-	len = 0;
 	result = NULL;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1);
+	len = ft_strlen_nullcrashless(s1);
 	len += n;
 	if (!(result = ft_strnew(len)))
 		return (NULL);
-	result = ft_strcpy(result, s1);
-	result = ft_strncat(result, s2, n);
+	result = ft_strcpy_nullcrashless(result, s1);
+	result = ft_strncat_nullcrashless(result, s2, n);
 	return (result);
 }
