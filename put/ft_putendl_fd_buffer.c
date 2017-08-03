98b8d90ab0dc8d92bd8103894fb175254a6948ff
Mathis PANDELE <mapandel@e1r5p22.42.fr>
2017-08-03 07:14:39 +0200

    ft_putendl_fd_buffer.c 2.1 : Change return value

--- put/ft_putendl_fd_buffer.c
+++ put/ft_putendl_fd_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 06:33:20 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/02 06:34:42 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/03 02:44:04 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_putendl_fd_buffer(char *s, int release, int fd)
 	static char		*buf;
 
 	if (!(buf = ft_strjoin_leakless(buf, s)))
-		return (1);
+		return (-1);
 	if (release)
 	{
 		ft_putendl_fd(buf, fd);
