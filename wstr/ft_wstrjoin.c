35164869b8cb2816a44dec555470e05551d326ce
Mathis PANDELE <mapandel@e1r5p22.42.fr>
2017-08-03 07:24:28 +0200

    ft_wstrjoin.c 2.1 : Null parameters work

--- wstr/ft_wstrjoin.c
+++ wstr/ft_wstrjoin.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 05:05:41 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/18 05:08:31 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/03 06:32:26 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ wchar_t			*ft_wstrjoin(wchar_t const *s1, wchar_t const *s2)
 	size_t		len;
 	wchar_t		*result;
 
-	len = 0;
 	result = NULL;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_wstrlen(s1);
-	len += ft_wstrlen(s2);
+	len = ft_wstrlen_nullcrashless(s1);
+	len += ft_wstrlen_nullcrashless(s2);
 	if (!(result = ft_wstrnew(len)))
 		return (NULL);
-	result = ft_wstrcpy(result, s1);
-	result = ft_wstrcat(result, s2);
+	result = ft_wstrcpy_nullcrashless(result, s1);
+	result = ft_wstrcat_nullcrashless(result, s2);
 	return (result);
 }
