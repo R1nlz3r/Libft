074f0714cde8181ebb4a75b2da9bc5790d3121a7
Mathis PANDELE <mapandel@e1r5p22.42.fr>
2017-08-03 08:31:53 +0200

    ft_putwstr_fd_buffer.c 2.0 : Implementation

---
+++ put/ft_putwstr_buffer.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putwstr_buffer.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/03 06:37:22 by mapandel          #+#    #+#             */
+/*   Updated: 2017/08/03 06:37:41 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_putwstr_buffer(wchar_t *wc, int release)
+{
+	static wchar_t	*buf;
+
+	if (!(buf = ft_wstrjoin_leakless(buf, wc)))
+		return (-1);
+	if (release)
+	{
+		ft_putwstr(buf);
+		ft_wstrdel(&buf);
+	}
+	return (0);
+}
