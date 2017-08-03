496cdbc6e9941d217c6cd44eb240ca179be82735
Mathis Pandelé <mapandel@student.42.fr>
2017-08-03 02:04:16 +0200

    ft_strnjoin_leakless.c 2.1 : Null parameters work

--- str/ft_strnjoin_leakless.c
+++ str/ft_strnjoin_leakless.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/01 13:05:45 by mapandel          #+#    #+#             */
-/*   Updated: 2017/05/01 13:06:58 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/03 01:44:14 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ char	*ft_strnjoin_leakless(char *s1, char const *s2, size_t n)
 	size_t		len;
 	char		*result;
 
-	len = 0;
 	result = NULL;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1);
+	len = ft_strlen_nullcrashless(s1);
 	len += n;
 	if (!(result = ft_strnew(len)))
 		return (NULL);
-	result = ft_strcpy(result, s1);
-	result = ft_strncat(result, s2, n);
-	ft_memdel((void**)&s1);
+	result = ft_strcpy_nullcrashless(result, s1);
+	result = ft_strncat_nullcrashless(result, s2, n);
+	if (s1)
+		ft_strdel(&s1);
 	return (result);
 }
