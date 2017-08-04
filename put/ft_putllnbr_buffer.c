901b485ee8f39242db5023f2438f6ce094eda45d
Mathis PANDELE <mapandel@e2r1p5.42.fr>
2017-08-04 11:14:13 +0200

    ft_putllnbr_buffer.c 2.1 : Fix

--- put/ft_putllnbr_buffer.c
+++ put/ft_putllnbr_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 09:54:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/04 10:40:46 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/04 11:10:55 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_putllnbr_buffer(long long nb, int release)
 {
 	char	*s;
 
-	if (!(s = ft_strbuffer_leakless(ft_lltoabase_signed(nb))))
+	if (!(s = ft_strbuffer_leakless(ft_lltoabase_signed(nb, 10))))
 		return (-1);
 	if (release)
 		ft_putstr(s);
