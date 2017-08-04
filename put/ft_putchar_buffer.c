ff1d0c1bd334a1e760e9ec121cc2039c9a21a402
Mathis PANDELE <mapandel@e2r1p5.42.fr>
2017-08-04 11:13:41 +0200

    ft_putchar_buffer.c 2.3 : Fix

--- put/ft_putchar_buffer.c
+++ put/ft_putchar_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 03:56:55 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/04 10:42:41 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/04 11:06:57 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_putchar_buffer(char c, int release)
 {
 	char	*s;
 
-	if (!s = ft_strnew(1))
+	if (!(s = ft_strnew(1)))
 		return (-1);
 	s[0] = c;
 	if (!(s = ft_strbuffer_leakless(s)))
