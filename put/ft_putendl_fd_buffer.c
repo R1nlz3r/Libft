a082b338464c8ac987dc15bf4c10350be628d4f0
Mathis Pandelé <mapandel@student.42.fr>
2017-08-04 10:43:28 +0200

    ft_putendl_fd_buffer.c 2.2 : Using ft_strbuffer

--- put/ft_putendl_fd_buffer.c
+++ put/ft_putendl_fd_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 06:33:20 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/03 02:44:04 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/04 08:01:45 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int		ft_putendl_fd_buffer(char *s, int release, int fd)
 {
-	static char		*buf;
-
-	if (!(buf = ft_strjoin_leakless(buf, s)))
+	if (!(s = ft_strbuffer_leakless(s)))
 		return (-1);
 	if (release)
 	{
-		ft_putendl_fd(buf, fd);
-		ft_strdel(&buf);
+		ft_putendl_fd(s, fd);
+		ft_strdel(&s);
 	}
 	return (0);
 }
