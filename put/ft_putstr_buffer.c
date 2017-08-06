152560035f8cd0df1a3e0df713670c65826ef419
Mathis Pandelé <mapandel@student.42.fr>
2017-08-06 21:45:12 +0200

    ft_putstr_buffer.c 2.3 : CrashFix

--- put/ft_putstr_buffer.c
+++ put/ft_putstr_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 06:35:53 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/04 07:02:22 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/06 19:44:27 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_putstr_buffer(char *s, int release)
 {
-	if (!(s = ft_strbuffer_leakless(s)))
+	if (!(s = ft_strbuffer(s)))
 		return (-1);
 	if (release)
 	{
