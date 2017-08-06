4df0d6d1589bc4a41c3b47eebdc13bb77eca36ef
Mathis Pandelé <mapandel@student.42.fr>
2017-08-06 22:30:45 +0200

    ft_putwstr_fd_buffer.c 2.1 : Using ft_wstrbuffer.c

--- put/ft_putwstr_fd_buffer.c
+++ put/ft_putwstr_fd_buffer.c
@@ -6,22 +6,18 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 06:38:13 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/03 06:38:34 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/06 21:06:53 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_putwstr_fd_buffer(wchar_t *wc, int release, int fd)
+int		ft_putwstr_fd_buffer(wchar_t *ws, int release, int fd)
 {
-	static wchar_t	*buf;
-
-	if (!(buf = ft_wstrjoin_leakless(buf, wc)))
+	if (!(ws = ft_wstrbuffer(ws)))
 		return (-1);
 	if (release)
-	{
-		ft_putwstr_fd(buf, fd);
-		ft_wstrdel(&buf);
-	}
+		ft_putwstr_fd(ws, fd);
+	ft_wstrdel(&ws);
 	return (0);
 }
