43922b98d88c1f9aae42179712c7d200782613fb
Mathis Pandelé <mapandel@student.42.fr>
2017-08-06 21:40:23 +0200

    ft_putchar_fd_buffer.c 2.4 : Leakfix

--- put/ft_putchar_fd_buffer.c
+++ put/ft_putchar_fd_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/02 04:40:01 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/04 08:26:02 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/06 06:26:55 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@ int		ft_putchar_fd_buffer(char c, int release, int fd)
 {
 	char	*s;
 
-	if (!s = ft_strnew(1))
+	if (!(s = ft_strnew(1)))
 		return (-1);
 	s[0] = c;
 	if (!(s = ft_strbuffer_leakless(s)))
 		return (-1);
 	if (release)
 		ft_putstr_fd(s, fd);
-	ft_strdel(&s);
 	return (0);
 }
