cbfa5f26a0c86dade08b3877318f0c2f2f53e6da
Mathis PANDELE <mapandel@e2r7p16.42.fr>
2017-08-11 06:11:23 +0200

    ft_putwstr_fd_buffer.c 2.2 : Fix

--- put/ft_putwstr_fd_buffer.c
+++ put/ft_putwstr_fd_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 06:38:13 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/06 21:06:53 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/11 05:57:40 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int		ft_putwstr_fd_buffer(wchar_t *ws, int release, int fd)
 	if (!(ws = ft_wstrbuffer(ws)))
 		return (-1);
 	if (release)
+	{
 		ft_putwstr_fd(ws, fd);
-	ft_wstrdel(&ws);
+		ft_wstrclr(ws);
+	}
 	return (0);
 }
