e44443ef751fe351005a3c69158f4454b148e781
Mathis PANDELE <mapandel@e2r7p16.42.fr>
2017-08-11 06:09:50 +0200

    ft_putwendl_fd_buffer.c 2.2 : Fix

--- put/ft_putwendl_fd_buffer.c
+++ put/ft_putwendl_fd_buffer.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 06:33:55 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/06 20:10:50 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/11 05:59:00 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int		ft_putwendl_fd_buffer(wchar_t *ws, int release, int fd)
 	if (!(ws = ft_wstrbuffer(ws)))
 		return (-1);
 	if (release)
+	{
 		ft_putwendl_fd(ws, fd);
-	ft_wstrdel(&ws);
+		ft_wstrclr(ws);
+	}
 	return (0);
 }
