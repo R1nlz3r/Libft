dbe5d8253c652d34d70b41141301034c9b5f8983
Mathis Pandelé <mapandel@student.42.fr>
2017-08-19 19:06:59 +0200

    ft_atoi_iter.c 2.1 : Fix

--- conv/ft_atoi_iter.c
+++ conv/ft_atoi_iter.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 13:27:20 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/19 13:40:58 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/19 19:06:43 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int		ft_atoi_iter(const char *nptr, size_t *iter)
 	while (ft_isspace(nptr[*iter]))
 		(*iter)++;
 	if (nptr[*iter] == '+' || nptr[*iter] == '-')
-		if (nptr[*iter++] == '-')
+		if (nptr[(*iter)++] == '-')
 			neg = -1;
 	while (nptr[*iter] >= '0' && nptr[*iter] <= '9')
-		result = result * 10 + nptr[*iter++] - '0';
+		result = result * 10 + nptr[(*iter)++] - '0';
 	return (result * neg);
 }
