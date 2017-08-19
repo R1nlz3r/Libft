5c3f561cdff61e154266d1718e79279c584b4acf
Mathis Pandelé <mapandel@student.42.fr>
2017-08-19 19:07:40 +0200

    ft_atoi_spacesless_iter.c 2.1 : Fix

--- conv/ft_atoi_spacesless_iter.c
+++ conv/ft_atoi_spacesless_iter.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 13:38:44 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/19 13:39:52 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/19 19:07:15 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int		ft_atoi_spacesless_iter(const char *nptr, size_t *iter)
 	neg = 1;
 	result = 0;
 	if (nptr[*iter] == '+' || nptr[*iter] == '-')
-		if (nptr[*iter++] == '-')
+		if (nptr[(*iter)++] == '-')
 			neg = -1;
 	while (nptr[*iter] >= '0' && nptr[*iter] <= '9')
-		result = result * 10 + nptr[*iter++] - '0';
+		result = result * 10 + nptr[(*iter)++] - '0';
 	return (result * neg);
 }
