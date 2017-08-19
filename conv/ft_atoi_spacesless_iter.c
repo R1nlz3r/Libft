20c545c4f267f6ebd914a6ff8c316494f23fae22
Mathis Pandelé <mapandel@student.42.fr>
2017-08-19 13:43:22 +0200

    ft_atoi_spacesless_iter.c 2.0 : Implementation

---
+++ conv/ft_atoi_spacesless_iter.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_spacesless_iter.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/19 13:38:44 by mapandel          #+#    #+#             */
+/*   Updated: 2017/08/19 13:39:52 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi_spacesless_iter(const char *nptr, size_t *iter)
+{
+	int			neg;
+	int			result;
+
+	neg = 1;
+	result = 0;
+	if (nptr[*iter] == '+' || nptr[*iter] == '-')
+		if (nptr[*iter++] == '-')
+			neg = -1;
+	while (nptr[*iter] >= '0' && nptr[*iter] <= '9')
+		result = result * 10 + nptr[*iter++] - '0';
+	return (result * neg);
+}
