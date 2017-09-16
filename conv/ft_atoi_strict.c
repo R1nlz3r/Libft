9c4d0101b804d1b766c57b1e774eef1a320f6342
Mathis Pandelé <mapandel@student.42.fr>
2017-09-16 09:33:06 +0200

    ft_atoi_strict.c 2.1 : Fix significant numbers

--- conv/ft_atoi_strict.c
+++ conv/ft_atoi_strict.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 21:00:49 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/20 21:11:28 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/16 09:28:39 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,29 @@
 
 long		ft_atoi_strict(const char *nptr)
 {
+	size_t	zero;
+	size_t	len;
+	size_t	i;
+
+	zero = 0;
 	if (!nptr[0]
 		|| !((ft_isdigit(nptr[0]) || nptr[0] == '+'
 		|| nptr[0] == '-') && (ft_strisdigit(&nptr[1])
-		|| (!nptr[1] && ft_isdigit(nptr[0]))))
-		|| ft_strlen(nptr) > 11
-		|| (ft_strlen(nptr) == 11 && ft_isdigit(nptr[0]))
-		|| (ft_strlen(nptr) == 10
+		|| (!nptr[1] && ft_isdigit(nptr[0])))))
+		return (10000000000);
+	len = ft_strlen(nptr);
+	if ((i = 1) && nptr[0] == '0')
+		++zero;
+	while (i < len && nptr[i++] == '0')
+		++zero;
+	if (len > 11 + zero
+		|| (len == 11 + zero && ft_isdigit(nptr[0]))
+		|| (len == 10 + zero
 		&& ft_strcmp(nptr, "2147483647") > 0)
-		|| (ft_strlen(nptr) == 11 && nptr[0] == '+'
-		&& ft_strcmp(&nptr[1], "2147483647") > 0)
-		|| (ft_strlen(nptr) == 11 && nptr[0] == '-'
-		&& ft_strcmp(&nptr[1], "2147483648") > 0))
+		|| (len == 11 + zero && nptr[0] == '+'
+		&& ft_strcmp(&nptr[1 + zero], "2147483647") > 0)
+		|| (len == 11 + zero && nptr[0] == '-'
+		&& ft_strcmp(&nptr[1 + zero], "2147483648") > 0))
 		return (10000000000);
 	return ((int)ft_atoi(nptr));
 }
