927c6722d0d9645a511de392785a69c77123dd65
Mathis Pandelé <mapandel@student.42.fr>
2017-09-21 20:19:02 +0200

    ft_puterror.c 2.0 : Implementation

---
+++ put/ft_puterror.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puterror.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/21 19:40:27 by mapandel          #+#    #+#             */
+/*   Updated: 2017/09/21 20:09:50 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_puterror(char *name, char *reason)
+{
+	if (name)
+	{
+		ft_putstr_fd(name, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	if (reason)
+	{
+		ft_putstr_fd(reason, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putendl_fd(strerror(errno), 2);
+}
