6dcb7fefc4813addabc9444bd74266ac5f23bed8
Mathis Pandelé <mapandel@student.42.fr>
2017-09-21 20:19:18 +0200

    ft_puterror_exit.c 2.0 : Implementation

---
+++ put/ft_puterror_exit.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puterror_exit.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/21 20:09:53 by mapandel          #+#    #+#             */
+/*   Updated: 2017/09/21 20:10:28 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_puterror_exit(char *name, char *reason)
+{
+	if (name)
+	{
+		ft_putstr_fd(name, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	if (reason)
+	{
+		ft_putstr_fd(reason, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putendl_fd(strerror(errno), 2);
+	exit(-1);
+}
