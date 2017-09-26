1e535aa20137a7510637542bcaba48ada6eff658
Mathis Pandelé <mapandel@student.42.fr>
2017-09-26 12:14:59 +0200

    ft_lstnew.c 2.1 : Secure init

--- lst/ft_lstnew.c
+++ lst/ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mapandel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 23:38:12 by mapandel          #+#    #+#             */
-/*   Updated: 2016/12/26 14:35:45 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/26 08:34:25 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	t_list		*new;
 
 	new = NULL;
-	if (!(new = (t_list*)malloc(sizeof(t_list))))
+	if (!(new = ft_memalloc(sizeof(t_list))))
 		return (NULL);
 	if (!content)
 	{
@@ -26,7 +26,7 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	}
 	else
 	{
-		if (!(new->content = malloc(content_size)))
+		if (!(new->content = ft_memalloc(content_size)))
 			return (NULL);
 		new->content = ft_memcpy(new->content, content, content_size);
 		new->content_size = content_size;
