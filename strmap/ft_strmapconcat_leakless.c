33f0583012c5a59e5055be5e7e1faf7e7a89f063
Mathis Pandelé <mapandel@student.42.fr>
2018-01-15 06:50:46 +0100

    ft_strmapconcat_leakless.c 2.1 : New path

---
+++ strmap/ft_strmapconcat_leakless.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapconcat_leakless.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/26 09:25:24 by mapandel          #+#    #+#             */
+/*   Updated: 2017/09/28 11:16:28 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**ft_strmapconcat_leakless(char **m, size_t size)
+{
+	char		**result;
+	size_t		i;
+	size_t		j;
+
+	if (!m || !(result = ft_memalloc(sizeof(char*) * (size + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		if (m[i])
+			result[j++] = ft_strdup(m[i]);
+		ft_strdel(&m[i]);
+		++i;
+	}
+	ft_memdel((void**)&m);
+	return (result);
+}
