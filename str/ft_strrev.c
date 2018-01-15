45245b27adb800cc4dca833d94f3d823178a76c4
Mathis Pandelé <mapandel@student.42.fr>
2018-01-15 07:12:13 +0100

    ft_strrev.c 2.1 : Secure

--- str/ft_strrev.c
+++ str/ft_strrev.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/21 02:23:19 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/03 13:33:38 by mapandel         ###   ########.fr       */
+/*   Updated: 2018/01/15 07:00:57 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ char	*ft_strrev(const char *s)
 	size_t		start;
 	size_t		end;
 
-	dup = ft_strdup(s);
+	if (!(dup = ft_strdup(s)))
+		return (NULL);
 	start = 0;
 	end = ft_strlen(dup) - 1;
 	while (dup && dup[start] && start < end)
