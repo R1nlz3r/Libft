3e5a3de8d2f6d2b7ef5e24e90ac787a82ce164f7
Mathis Pandelé <mapandel@student.42.fr>
2018-01-16 07:37:25 +0100

    ft_atoll.c 2.0 : Implementation

---
+++ conv/ft_atoll.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/16 04:39:09 by mapandel          #+#    #+#             */
+/*   Updated: 2018/01/16 04:57:46 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long long	ft_atoll(const char *nptr)
+{
+	size_t		i;
+	int			neg;
+	long long	result;
+
+	i = 0;
+	neg = 1;
+	result = 0;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		if (nptr[i++] == '-')
+			neg = -1;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+		result = result * 10 + nptr[i++] - '0';
+	return (result * neg);
+
+}
