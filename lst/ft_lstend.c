48bdbea7261ac746a734eedba451bbce08fbb1bf
R1nlz3r <mapandel@student.42.fr>
2019-11-19 18:29:47 +0100

    Issue #6: New functions lststart/end + lstinsert

--- lst/ft_lstend.c
+++ lst/ft_lstend.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstend.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/07 02:03:10 by mapandel          #+#    #+#             */
-/*   Updated: 2019/11/19 02:11:01 by mapandel         ###   ########.fr       */
+/*   Created: 2019/11/19 10:14:03 by mapandel          #+#    #+#             */
+/*   Updated: 2019/11/19 18:01:01 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lst.h"
 
 /*
-**	ft_lstadd: list addition
-**		Adds a t_list* node to the start of a chained t_list*
-**			pointed by its address
-**		The starting pointer this chained list is replaced by
-**			the address of the added node
-**		The function do not return a value
+**	ft_lstend: list end
+**		Finds the ending node of a t_list* chain and returns it
+**		NULL behaviors are handled
 */
 
-void	ft_lstadd(t_list **alst, t_list *new)
-{
-	new->next = *alst;
-	*alst = new;
+t_list		*ft_lstend(t_list *lst) {
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
---
+++ lst/ft_lstinsertaft.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstinsertaft.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/19 10:23:41 by mapandel          #+#    #+#             */
+/*   Updated: 2019/11/19 18:12:22 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lst.h"
+
+/*
+**	ft_lstinsertaft: list insertion after
+**		Inserts a t_list* chain into an other
+**		This new chain is plugged right on the following node
+**			of the first parameter
+**		And stops at the end of the second, assuming it is the starting node
+**		NULL behaviors are handled
+**		The function do not return a value
+*/
+
+void	ft_lstinsertaft(t_list *lst, t_list *new)
+{
+	t_list		*aft;
+	t_list		*new_end;
+
+	if (!lst)
+		return ;
+	aft = NULL;
+	new_end = ft_lstend(new);
+	if (lst->next)
+	{
+		aft = lst->next;
+		aft->previous = new_end;
+	}
+	lst->next = new;
+	if (!new)
+		return ;
+	new->previous = lst;
+	new_end->next = aft;
+}
